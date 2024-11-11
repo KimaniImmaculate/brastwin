54a5da78f668348a173962d376156535baf09fc7
KimaniImmaculate <immakym001@gmail.com>
2024-11-11 19:17:49 +0300

    js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("Script is running");
+
 document.getElementById("order-form").addEventListener("submit", function(event)
 {
     event.preventDefault();
@@ -8,3 +10,47 @@ document.getElementById("success-message").classList.remove("hidden");
 
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+
+const products=[
+    {
+        name:"Timber",
+        price: 20,
+        description: "High-quality timber for construction.",
+        image: "images/TIMBER.jpg"
+    },
+    {
+        name:"Beams",
+        price: 18,
+        description: "Sturdy beams for support structures.",
+        image: "images/BEAMS.jpg"
+    },
+    {
+        name:"Furniture",
+        price: 12,
+        description: "Beautifully crafted wooden furniture.",
+        image: "images/FURNITURE.jpg"
+    },
+];
+
+function displayProducts(){
+    const productList = document.getElementById("product-list")
+
+    products.forEach(product => {
+        const productCard =document.createElement("div");
+        productCard.classList.className = "product-card";
+
+        productCard.innerHtml = `
+        <h3>${product.name}</h3>
+        <p>${product.price}</p>
+        <p>${product.description}</p>
+        <button class= "order-now">Order Now</button>
+        `;
+
+        productList.appendChild(productCard);
+    });
+}
+
+displayProducts();
+
+});
