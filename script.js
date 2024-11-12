0f532812db561ed12b1fe96fd2e4d8d8ffd34e82
KimaniImmaculate <immakym001@gmail.com>
2024-11-12 17:34:08 +0300

    js

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.getElementById("success-message").classList.remove("hidden");
 
 document.addEventListener("DOMContentLoaded", function() {
 
+//Add products
 const products=[
     {
         name:"Timber",
@@ -37,16 +38,33 @@ function displayProducts(){
     const productList = document.getElementById("product-list")
 
     products.forEach(product => {
+        //Product card container 
         const productCard =document.createElement("div");
         productCard.classList.className = "product-card";
 
-        productCard.innerHtml = `
-        <h3>${product.name}</h3>
-        <p>${product.price}</p>
-        <p>${product.description}</p>
-        <button class= "order-now">Order Now</button>
-        `;
+        //Add image
+        const img = document.createElement("img");
+        img.src = product.image;
+        img.alt = product.name;
+        img.className = "product-img";
+        productCard.appendChild(img);
 
+        //Add product name
+        const name = document.createElement("h3");
+        name.innerText = product.name;
+        productCard.appendChild(name);
+
+        //Add price
+        const price = document.createElement("p");
+        price.innerText = `Price: $${product.price}`;
+        productCard.appendChild(price);
+
+        //Add description
+        const description = document.createElement("p");
+        description.innerText = product.description;
+        productCard.appendChild(description);
+
+        //Append product card to the product list
         productList.appendChild(productCard);
     });
 }
